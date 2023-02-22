745f5d1feed1342215418b34773ba0dae6e58cbc
LesCamposR <lesthercamposramirez91@gmail.com>
2023-02-22 21:24:32 +0000

    First

--- src/app.js
+++ src/app.js
@@ -7,5 +7,25 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  //Hearts, Spades, Clubs and Diamonds
+
+  console.log("Everything is working!");
+  document.querySelector("button").addEventListener("click", execute);
 };
+let icons = ["\u2665", "\u2660", "\u2663", "\u2666"];
+let numbers = [
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "K",
+  "Q",
+  "a"
+];
+console.log(icons);
